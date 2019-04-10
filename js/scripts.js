c1ee4ad2cb72b1dae9236bbb6d580dc7dbd572a5
sabrina mianne <sabrina.epicodus@gmail.com>
2019-04-10 16:47:53 -0700

    Add the user logic to the project

---
+++ js/scripts.js
@@ -0,0 +1,34 @@
+$(document).ready(function () {
+
+var clickGame = document.getElementById("clickGame");
+var case1 = document.getElementById("corner1");
+var case2 = document.getElementById("side2");
+var case3 = document.getElementById("corner3");
+var case4 = document.getElementById("side4");
+var case5 = document.getElementById("center5");
+var case6 = document.getElementById("side6");
+var case7 = document.getElementById("corner7");
+var case8 = document.getElementById("side8");
+var case9 = document.getElementById("corner9");
+// var elt = this;
+// var idElt = this.getAttribute("id");
+// var iconCross = $("input#x").val();
+// var iconCircle = $("input#o").val();
+//
+// console.log(elt, idElt);
+
+
+case1.addEventListener('click', function(e) {
+    e.target.innerHTML = 'X';
+});
+
+// var game = {
+//   user: "",
+//   computer: "",
+//   currentPlayer: "",
+//   moves: 1,
+// }
+
+
+
+});
